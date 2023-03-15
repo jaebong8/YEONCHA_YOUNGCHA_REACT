f34cb859d9fa979b0b17a7d2353bdf9202764b30
jaebong8 <jaebong90@naver.com>
2023-03-15 18:01:53 +0900

    달력에 결재 완료된 문서들 표시

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -15,6 +15,13 @@ import {
     isSaturday,
     isSunday,
 } from "date-fns";
+import { useOutletContext } from "react-router-dom";
+import { UserType } from "types/ts";
+import { collection } from "firebase/firestore";
+import { useFirestoreQueryData } from "@react-query-firebase/firestore";
+import { db } from "firebaseConfig/firebase";
+import { timeUid } from "utils/common";
+import { Box } from "@chakra-ui/react";
 
 const Calendar = () => {
     const [currentDate, setCurrentDate] = useState(new Date());
@@ -23,6 +30,27 @@ const Calendar = () => {
     const startDate = startOfWeek(monthStart);
     const endDate = endOfWeek(monthEnd);
     const weekMock = ["일", "월", "화", "수", "목", "금", "토"];
+    const userInfo: UserType = useOutletContext().userInfo;
+    const userUid = useOutletContext().userUid;
+    const company = userInfo?.company;
+
+    const adminDocRef = collection(db, company);
+    const docsQuery = useFirestoreQueryData([company, timeUid()], adminDocRef, { subscribe: true })?.data;
+
+    const docsInfo = useMemo(() => {
+        if (docsQuery !== undefined) {
+            const mergeObj = docsQuery?.[0];
+            for (let i = 1; i < docsQuery.length; i++) {
+                Object.assign(mergeObj, docsQuery?.[i]);
+            }
+
+            const adminArray: any[] = Object.values({ ...mergeObj });
+            const filterArray = adminArray.filter((v) => v.status === "success");
+            console.log(filterArray);
+            return filterArray;
+        }
+    }, [docsQuery]);
+
     const nextMonthHandler = useCallback(() => {
         setCurrentDate(addMonths(currentDate, 1));
     }, [currentDate]);
@@ -36,6 +64,7 @@ const Calendar = () => {
             monthArray.push(day);
             day = addDays(day, 1);
         }
+
         return monthArray;
     }, [startDate, endDate]);
     return (
@@ -72,9 +101,11 @@ const Calendar = () => {
             </div>
             <div className={styles.dateContainer}>
                 {createMonth.map((v, i) => {
+                    const date = format(v, "yyyy/MM/dd");
                     let style;
                     const validation = getMonth(currentDate) === getMonth(v);
                     const today = format(new Date(), "yyyyMMdd") === format(v, "yyyyMMdd");
+
                     if (validation && isSaturday(v)) {
                         style = {
                             color: "blue",
@@ -84,6 +115,7 @@ const Calendar = () => {
                             color: "red",
                         };
                     }
+
                     return (
                         <div
                             key={`date${i}`}
@@ -94,6 +126,68 @@ const Calendar = () => {
                                 <span className={styles.day}>{format(v, "d")}</span>
                                 {today && <span className={styles.today}>(오늘)</span>}
                             </div>
+                            <Box position="absolute" w="100%" zIndex="9999">
+                                {docsInfo?.map((doc) => {
+                                    if (
+                                        differenceInCalendarDays(new Date(doc.startDate), v) <= 0 &&
+                                        differenceInCalendarDays(new Date(doc.endDate), v) >= 0
+                                    ) {
+                                        if (
+                                            date === doc.startDate &&
+                                            differenceInCalendarDays(
+                                                new Date(doc.endDate),
+                                                endOfWeek(new Date(doc.startDate))
+                                            ) <= 0
+                                        ) {
+                                            const width =
+                                                differenceInCalendarDays(
+                                                    new Date(doc.endDate),
+                                                    new Date(doc.startDate)
+                                                ) + 1;
+
+                                            return (
+                                                <AnnualBox doc={doc} width={width} key={`${doc.documentUid}+${v}`} />
+                                            );
+                                        }
+                                        if (
+                                            date === doc.startDate &&
+                                            differenceInCalendarDays(
+                                                new Date(doc.endDate),
+                                                endOfWeek(new Date(doc.startDate))
+                                            ) >= 0
+                                        ) {
+                                            const width =
+                                                differenceInCalendarDays(
+                                                    endOfWeek(new Date(doc.startDate)),
+                                                    new Date(doc.startDate)
+                                                ) + 1;
+                                            return (
+                                                <AnnualBox doc={doc} width={width} key={`${doc.documentUid}+${v}`} />
+                                            );
+                                        }
+
+                                        if (
+                                            isSunday(v) &&
+                                            differenceInCalendarDays(new Date(doc.endDate), endOfWeek(new Date(v))) <= 0
+                                        ) {
+                                            const width = differenceInCalendarDays(new Date(doc.endDate), v) + 1;
+                                            return (
+                                                <AnnualBox doc={doc} width={width} key={`${doc.documentUid}+${v}`} />
+                                            );
+                                        }
+
+                                        if (
+                                            isSunday(v) &&
+                                            differenceInCalendarDays(new Date(doc.endDate), endOfWeek(new Date(v))) >= 0
+                                        ) {
+                                            const width = differenceInCalendarDays(endOfWeek(new Date(v)), v) + 1;
+                                            return (
+                                                <AnnualBox doc={doc} width={width} key={`${doc.documentUid}+${v}`} />
+                                            );
+                                        }
+                                    }
+                                })}
+                            </Box>
                         </div>
                     );
                 })}
@@ -103,3 +197,19 @@ const Calendar = () => {
 };
 
 export default Calendar;
+
+const AnnualBox = ({ width, doc }: { width: number; doc: any }) => {
+    return (
+        <Box
+            width={`${100 * width}%`}
+            bg="gray.500"
+            textAlign="center"
+            color="#fff"
+            fontWeight="bold"
+            ml={`${width - 1}px`}
+            mt="1px"
+        >
+            {doc.name}
+        </Box>
+    );
+};
